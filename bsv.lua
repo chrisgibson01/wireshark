57279c0ddc5bebfc715916c3d44fc4f7d316bcaa
Chris Gibson <chris@gibsonic.com>
2020-01-10 16:22:38 +0000

    dissecting output scripts

--- bsv.lua
+++ bsv.lua
@@ -3,6 +3,7 @@ bsv_protocol = Proto("BSV",  "Bitcoin SV Protocol")
 bsv_protocol.fields = {}
 
 function bsv_protocol.dissector(buffer, pinfo, tree)
+    assert(false)
   length = buffer:len()
   if length == 0 then return end
 
@@ -11,5 +12,13 @@ function bsv_protocol.dissector(buffer, pinfo, tree)
   local subtree = tree:add(bsv_protocol, buffer(), "Bitcoin SV Protocol Data")
 end
 
-local bitcoin_port = DissectorTable.get("tcp.port")
-bitcoin_port:add(8333, bsv_protocol)
+--local tx_out_script_dissector = DissectorTable.get("bitcoin.tx.out.script")
+--tx_out_script_dissector:add(8333, bsv_protocol)
+
+local bitcoin_dissector = Dissector.get("bitcoin")
+assert(bitcoin_dissector)
+
+local bitcoin_dissector_table = DissectorTable.get("bitcoin.tx")
+assert(bitcoin_dissector_table)
+bitcoin_dissector_table:remove("bitcoin.tx.out.script")
+bitcoin_dissector_table:add("bitcoin.tx.out.script", bsv_protocol)
